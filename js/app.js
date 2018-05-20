a7ea2ba51667d3586b48ed411c25a8b4bab4f18d
Martina Masanova <martina.masanova@gmail.com>
2018-05-20 14:37:35 +0100

    Feat: create enemies

--- js/app.js
+++ js/app.js
@@ -1,24 +1,29 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    constructor(col = 0, row = 1, speed = 40) {
+        this.sprite = 'images/enemy-bug.png';
+        this.x = col * 101;
+        this.y = (row * 83) - 20;
+        this.speed = speed;
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 };
 
 // Now write your own player class
@@ -30,7 +35,7 @@ class Player {
         this.sprite = 'images/char-boy.pngs'
     }
 
-    update() {
+    update(dt) {
 
     }
 
@@ -39,7 +44,7 @@ class Player {
     }
 
     handleInput() {
-        
+
     }
 }
 
@@ -48,7 +53,36 @@ class Player {
 // Place the player object in a variable called player
 
 let allEnemies = [];
+
 const player = new Player();
+createEnemies();
+
+function createEnemies() {
+    const enemyPositions = [];
+
+    for (let enemyX = 0; enemyX < 3; enemyX++) {
+        let positionFound = false;
+        let position;
+        const speed = Math.floor(Math.random() * 181) + 50;
+
+        while (!positionFound) {
+            const col = Math.floor(Math.random() * 4);
+            const row = Math.floor(Math.random() * 3) + 1;
+            const existingPosition = enemyPositions.find((pos) => pos.col === col && pos.row === row)
+
+            position = {col, row};
+
+            if (existingPosition === undefined) {
+                enemyPositions.push(position);
+                positionFound = true;
+            }
+        }
+        
+        const enemy = new Enemy(position.col, position.row, speed);
+        allEnemies.push(enemy);
+    }
+}
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
